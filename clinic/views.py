61d86646760e6c8a869f123b01cc954500e9049f
srrobin07 <you@domain.com>
2020-02-15 16:37:37 +0600

    update

--- clinic/views.py
+++ clinic/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from .forms import OwnerForm,PetForm,VisitForm
-from .models import Owner,Pet
+from .models import Owner,Pet,Visit
 
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
