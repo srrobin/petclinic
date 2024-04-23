16b79dab8acbff4fddf9730d1763d56bffe9fb06
SR  ROBIN <srrobin07@gmail.com>
2024-04-23 17:12:02 +0600

    Update Axios.js 5000

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 import axios from "axios";
 
-const BASE_URL = "https://fake-server-6d3v.onrender.com";
-// const BASE_URL = "http://localhost:5000/";
+// const BASE_URL = "https://fake-server-6d3v.onrender.com";
+const BASE_URL = "http://localhost:5000/";
 export const AxiosInstance = axios.create({ baseURL: BASE_URL });
 
 export async function fetchOwner({ signal }) {
