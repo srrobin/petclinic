97c67f89dc1c8b76f90521ff152e0d3af34fc4cf
“srrobin” <srrobin07@gmail.com>
2024-07-29 12:07:40 +0600

    up

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,7 +1,8 @@
 /* eslint-disable max-len */
 import axios from "axios";
 
-const BASE_URL = "https://fake-server-6d3v.onrender.com";
+// const BASE_URL = "https://fake-server-6d3v.onrender.com";
+const BASE_URL = "https://waiting-tangible-letter.glitch.me";
 // const BASE_URL = "http://localhost:5000/";
 export const AxiosInstance = axios.create({ baseURL: BASE_URL });
 
